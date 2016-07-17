030e3ca10bc17858081ae42b96b836fb0c5f7a88
Alex Bennett <alexandergraybennett@gmail.com>
2016-07-17 10:02:57 -0600

    about to start trimming down

--- js/main.js
+++ js/main.js
@@ -1,41 +1,47 @@
 'use strict'
 
-var dotOne = $(".dot")[0];
-var dotTwo = $(".dot")[1];
-var dotThree = $(".dot")[2];
+var dotOne = $(".dot")[0]
+var dotTwo = $(".dot")[1]
+var dotThree = $(".dot")[2]
 var dots = [dotOne, dotTwo, dotThree]
 
-var slideOne = $(".slide")[0];
-var slideTwo = $(".slide")[1];
-var slideThree = $(".slide")[2];
+var d1 = $('.dot-one')
+var d2 = $('.dot-two')
+var d3 = $('.dot-three')
+
+var slideOne = $(".slide")[0]
+var slideTwo = $(".slide")[1]
+var slideThree = $(".slide")[2]
+var someSlides = [slideOne, slideTwo, slideThree]
 
 var s1 = $('.slide-one')
 var s2 = $('.slide-two')
 var s3 = $('.slide-three')
 
-var someSlides = [slideOne, slideTwo, slideThree];
+var h1 = $('.header-one')
+var h2 = $('.header-two')
+var h3 = $('.header-three')
+var features = $('h2').get()
 
-const slideOneInfo = slideOne.innerHTML;
-var features = $('h2').get();
+const slideOneInfo = slideOne.innerHTML
 
 class SlideShow {
 
   constructor(slides, features, dots){
-    this.slides = slides;
-    this.features = features;
-    this.dots = dots;
-    this.arrayOrder = '';
-    this.arrangements = [this.arrangementOne, this.arrangementTwo, this.arrangementThree];
-    this.stop = 
+    this.slides = slides
+    this.features = features
+    this.dots = dots
+    this.arrayOrder = ''
+    this.arrangements = [this.arrangementOne, this.arrangementTwo, this.arrangementThree]
   }
 
   setOrder(slides){
     this.slides.forEach((i) => {
       i.addEventListener('click', () => {
-        var curr = i;
-        this.slides.splice(i, 1);
-        this.slides.unshift(curr);
-        console.log(slides);
+        var curr = i
+        this.slides.splice(i, 1)
+        this.slides.unshift(curr)
+        console.log(slides)
       })
     })
   }
@@ -43,10 +49,9 @@ class SlideShow {
   hoverTitle(){
     this.features.forEach( (i) => {
       i.addEventListener('mouseover', () => {
-        clearInterval(this.tempo);
-        if(i.innerHTML === 'Slide one') { this.arrangementOne(); }
-        if(i.innerHTML === 'Slide two') { this.arrangementTwo(); }
-        if(i.innerHTML === 'Slide three') { this.arrangementThree(); }
+        if(i.innerHTML === 'Slide one') this.arrangementOne()
+        if(i.innerHTML === 'Slide two') this.arrangementTwo()
+        if(i.innerHTML === 'Slide three') this.arrangementThree()
       })
     })
   }
@@ -54,11 +59,9 @@ class SlideShow {
   hoverDots(){
     this.dots.forEach( (dot) => {
       dot.addEventListener('mouseover', () => {
-        console.log('clear timeout');
-        clearInterval(this.tempo);
-        if(dot === dotOne) { this.arrangementOne(); }
-        if(dot === dotTwo) { this.arrangementTwo(); }
-        if(dot === dotThree) { this.arrangementThree(); }
+        if(dot === dotOne) this.arrangementOne()
+        if(dot === dotTwo) this.arrangementTwo()
+        if(dot === dotThree) this.arrangementThree()
       })
     })
   }
@@ -66,46 +69,43 @@ class SlideShow {
   hoverSlides(){
     this.slides.forEach( (i) => {
       i.addEventListener('mouseover', () => {
-        clearInterval(this.tempo);
-        if(i === slideOne) this.arrangementOne();
-        if(i === slideTwo) this.arrangementTwo();
-        if(i === slideThree) this.arrangementThree();
+        if(i === slideOne) this.arrangementOne()
+        if(i === slideTwo) this.arrangementTwo()
+        if(i === slideThree) this.arrangementThree()
       })
     })
   }
 
   arrangementOne(){
-    s1.empty().append(slideOneInfo);
-    s1.css("background-color", "coral")
-    s2.css("background-color", "aqua")
-    s3.css("background-color", "gold")
+    s1.empty().append(slideOneInfo)
+    s1.css("background-color", "coral"); s2.css("background-color", "aqua"); s3.css("background-color", "gold")
+    d1.css('color', 'coral'); d2.css('color', 'black'); d3.css('color', 'black')
+    h1.css('color', 'coral'); h2.css('color', 'black'); h3.css('color', 'black')
   }
 
   arrangementTwo(){
     s1.empty().append(slideTwo.innerHTML);
-    s1.css("background-color", "aqua")
-    s2.css("background-color", "coral")
-    s3.css("background-color", "gold")
+    s1.css("background-color", "aqua"); s2.css("background-color", "coral"); s3.css("background-color", "gold")
+    d1.css('color', 'black'); d2.css('color', 'aqua'); d3.css('color', 'black')
+    h1.css('color', 'black'); h2.css('color', 'aqua'); h3.css('color', 'black')
   }
 
   arrangementThree(){
     s1.empty().append(slideThree.innerHTML);
-    s1.css("background-color", "gold")
-    s2.css("background-color", "aqua")
-    s3.css("background-color", "coral")
+    s1.css("background-color", "gold"); s2.css("background-color", "aqua"); s3.css("background-color", "coral")
+    d1.css('color', 'black'); d2.css('color', 'black'); d3.css('color', 'gold');
+    h1.css('color', 'black'); h2.css('color', 'black'); h3.css('color', 'gold')
   }
 
   automaticPagination() {
-    var intervalFunctions = this.arrangements;
+    var intervalFunctions = this.arrangements
     var intervalIndex = 0;
     var tempo = window.setInterval(function(){
-      intervalFunctions[intervalIndex++ % intervalFunctions.length]();
-    }, 2000);
+      intervalFunctions[intervalIndex++ % intervalFunctions.length]()
+    }, 7000);
     return tempo
   }
-
 }
-
 
 var carousel = new SlideShow(someSlides, features, dots)
 
@@ -113,12 +113,6 @@ carousel.hoverTitle();
 carousel.hoverDots();
 carousel.hoverSlides();
 carousel.setOrder(someSlides);
-// carousel.automaticPagination()
-
-someSlides.forEach(function(i){
-  i.addEventListener('mouseover', function(){
-    carousel.automaticPagination();
-  })
-})
+carousel.automaticPagination()
 
 console.log(features);
